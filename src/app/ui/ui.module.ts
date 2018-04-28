b50bf13f097a74bf03ad7c8768c34f83d73c95f9
btd1337 <helder.bertoldo@ice.ufjf.br>
2018-04-27 21:19:08 -0300

    0.4
    - Added Footer

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -5,13 +5,14 @@ import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   imports: [
     ClarityModule,
     CommonModule
   ],
-  declarations: [LayoutComponent, HeaderComponent, SidebarComponent, MainComponent],
+  declarations: [LayoutComponent, HeaderComponent, SidebarComponent, MainComponent, FooterComponent],
   exports: [LayoutComponent]
 })
 export class UiModule { }
